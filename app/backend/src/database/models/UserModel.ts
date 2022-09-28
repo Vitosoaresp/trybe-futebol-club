b2cceb8ec44c874e45cb10e3ee42cc98f2fbf6ef
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-27 22:25:03 -0300

    feat: create UsersModel

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,44 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  email!: string;
+  password!: string;
+  role!: string;
+}
+User.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: STRING,
+      allowNull: false,
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+    },
+    password: {
+      type: STRING,
+      allowNull: false,
+    },
+    role: {
+      type: STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'users',
+    timestamps: false,
+  },
+);
+
+export default User;
