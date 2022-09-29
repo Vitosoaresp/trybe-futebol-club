de89eaefac3f7b7c79bf00ff9cbfb1f14d69730a
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-29 15:25:22 -0300

    feat: interface Teams to service

---
+++ app/backend/src/interfaces/ITeamsServices.ts
@@ -0,0 +1,10 @@
+export interface ITeamsServices {
+  id: number;
+  teamName: string;
+}
+
+export interface ITeamsServicesReturn {
+  code: number;
+  message?: string;
+  data?: ITeamsServices[];
+}
