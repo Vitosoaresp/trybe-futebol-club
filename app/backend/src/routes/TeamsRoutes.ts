191915188a4d29b89b03f632e7ad5f9f470fc703
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-29 15:26:45 -0300

    feat: teams Router

---
+++ app/backend/src/routes/TeamsRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import TeamsController from '../controllers/TeamsController';
+
+const router = Router();
+
+router.get('/teams', TeamsController.getAll);
+
+export default router;
