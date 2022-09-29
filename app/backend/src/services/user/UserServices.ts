2d13c02492082954db2271fc7e9ca8a7bd76b43a
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-28 21:15:34 -0300

    feat: UserServices

--- app/backend/src/services/user/UserServices.ts
+++ app/backend/src/services/user/UserServices.ts
@@ -4,16 +4,18 @@ import BcryptService from '../utils/BcriptServices';
 
 export default class UserServices {
   static async login(email: string, password: string) {
-    const getUser = await User.findAll({
-      where: { email },
-    });
-    console.log(getUser);
-    if (getUser.length < 1 || !getUser) {
-      return { code: 404, message: 'User not found!' };
+    if (!email || !password) {
+      return { code: 400, message: 'All fields must be filled' };
     }
     const isValidDTO = validateLogin(email, password);
     if (!isValidDTO) {
       return { code: 401, message: 'Incorrect email or password' };
+    }
+    const getUser = await User.findAll({
+      where: { email },
+    });
+    if (getUser.length < 1 || !getUser) {
+      return { code: 404, message: 'User not found!' };
     }
     const checkPass = BcryptService.compare(getUser[0].password, password);
     if (!checkPass) {
