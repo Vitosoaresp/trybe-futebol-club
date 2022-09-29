678df3b15e7fa715f70ae9e1a701c3df3c6f43bc
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-29 15:44:27 -0300

    feat: create router get teams/:id

--- app/backend/src/routes/TeamsRoutes.ts
+++ app/backend/src/routes/TeamsRoutes.ts
@@ -5,5 +5,6 @@ import TeamsController from '../controllers/TeamsController';
 const router = Router();
 
 router.get('/teams', TeamsController.getAll);
+router.get('/teams/:id', TeamsController.getById);
 
 export default router;
