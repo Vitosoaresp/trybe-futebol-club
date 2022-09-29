5bfa05b937b2180554109a738c27114a48c079b9
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-28 21:46:15 -0300

    feat: test by login

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -4,7 +4,8 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
+import UserModel from '../database/models/UserModel';
+import { userLoginSend, userMock } from './mocks/User';
 
 import { Response } from 'superagent';
 
@@ -12,34 +13,36 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+describe('/LOGIN', () => {
+  before(async () => {
+    sinon.stub(UserModel, 'findAll').resolves([userMock] as UserModel[]);
+  });
 
-  // let chaiHttpResponse: Response;
+  after(() => {
+    (UserModel.findAll as sinon.SinonStub).restore();
+  });
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+  it('Deve fazer login com sucesso', async () => {
+    const result: Response = await chai
+      .request(app)
+      .post('/login')
+      .send(userLoginSend);
+    expect(result.status).to.equal(201);
+  });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+  it('Caso não seja passado um email, retorne a mensagem "All fields must be filled"', async () => {
+    const result: Response = await chai
+      .request(app)
+      .post('/login')
+      .send({ password: '' });
+    expect(result.status).to.equal(400);
+  });
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+  it('Caso o email não seja valido, retorne a mensagem "Incorrect email or password"', async () => {
+    const result: Response = await chai
+      .request(app)
+      .post('/login')
+      .send({ ...userLoginSend, email: 'email_invalido' });
+    expect(result.status).to.equal(401);
   });
 });
---
+++ app/backend/src/tests/mocks/User.ts
@@ -0,0 +1,13 @@
+const userLoginSend = {
+  email: 'admin@admin.com',
+  password: 'secret_admin',
+};
+
+const userMock = {
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+};
+
+export { userLoginSend, userMock };
