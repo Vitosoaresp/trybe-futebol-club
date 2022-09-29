fe5750af24ae460c7f7f3a9b11388c2668890a7b
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-28 22:21:01 -0300

    test: refac tests users

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -14,35 +14,80 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('/LOGIN', () => {
-  before(async () => {
-    sinon.stub(UserModel, 'findAll').resolves([userMock] as UserModel[]);
+  describe('caso passe email e password validos', () => {
+    before(async () => {
+      sinon.stub(UserModel, 'findAll').resolves([userMock] as UserModel[]);
+    });
+
+    after(() => {
+      (UserModel.findAll as sinon.SinonStub).restore();
+    });
+
+    it('Deve fazer login com sucesso e retornar um token', async () => {
+      const result: Response = await chai
+        .request(app)
+        .post('/login')
+        .send(userLoginSend);
+      expect(result.status).to.equal(201);
+      expect(result.body).to.have.keys('token');
+    });
   });
 
-  after(() => {
-    (UserModel.findAll as sinon.SinonStub).restore();
-  });
+  describe('caso passem email ou password invalidos', () => {
+    before(async () => {
+      sinon.stub(UserModel, 'findAll').resolves([userMock] as UserModel[]);
+    });
 
-  it('Deve fazer login com sucesso', async () => {
-    const result: Response = await chai
-      .request(app)
-      .post('/login')
-      .send(userLoginSend);
-    expect(result.status).to.equal(201);
-  });
+    after(() => {
+      (UserModel.findAll as sinon.SinonStub).restore();
+    });
 
-  it('Caso não seja passado um email, retorne a mensagem "All fields must be filled"', async () => {
-    const result: Response = await chai
-      .request(app)
-      .post('/login')
-      .send({ password: '' });
-    expect(result.status).to.equal(400);
-  });
+    it('se email não for passado', async () => {
+      const result: Response = await chai
+        .request(app)
+        .post('/login')
+        .send({ password: 'test123' });
+      expect(result.body).to.have.key('message');
+      expect(result.status).to.equal(400);
+      expect(result.body).to.have.equal({
+        message: 'All fields must be filled',
+      });
+    });
 
-  it('Caso o email não seja valido, retorne a mensagem "Incorrect email or password"', async () => {
-    const result: Response = await chai
-      .request(app)
-      .post('/login')
-      .send({ ...userLoginSend, email: 'email_invalido' });
-    expect(result.status).to.equal(401);
+    it('se password não for passado', async () => {
+      const result: Response = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: 'test123@gmail.com' });
+      expect(result.status).to.equal(400);
+      expect(result.body).to.have.key('message');
+      expect(result.body).to.have.equal({
+        message: 'All fields must be filled',
+      });
+    });
+
+    it('Caso o email não seja valido', async () => {
+      const result: Response = await chai
+        .request(app)
+        .post('/login')
+        .send({ ...userLoginSend, email: 'email_invalido' });
+      expect(result.status).to.equal(401);
+      expect(result.body).to.have.key('message');
+      expect(result.body).to.have.equals({
+        message: 'Incorrect email or password',
+      });
+    });
+
+    it('Caso a senha esteja incorreta', async () => {
+      const result: Response = await chai
+        .request(app)
+        .post('/login')
+        .send({ ...userLoginSend, password: 'senhaqualqur' });
+      expect(result.status).to.be.equal(401);
+      // expect(result.body).to.have.key('message');
+      expect(result.body).to.have.equal({
+        message: 'Incorrect email or password',
+      });
+    });
   });
 });
