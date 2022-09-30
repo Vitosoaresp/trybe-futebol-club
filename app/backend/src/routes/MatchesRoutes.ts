97ba42a7e9ed2f3f5997d73e3c2607159abd1f5f
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-30 18:30:50 -0300

    feat: router patch update goal

--- app/backend/src/routes/MatchesRoutes.ts
+++ app/backend/src/routes/MatchesRoutes.ts
@@ -8,5 +8,6 @@ const router = Router();
 router.get('/matches', MatchesController.getAll);
 router.post('/matches', Auth.authToken, MatchesController.create);
 router.patch('/matches/:id/finish', MatchesController.update);
+router.patch('/matches/:id', MatchesController.updateGoals);
 
 export default router;
