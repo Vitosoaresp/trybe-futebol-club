4d5ac88153c472f4c870f40321ba08040ac3009e
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-30 16:13:51 -0300

    test: create test post

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -5,12 +5,16 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import MatchesModel from '../database/models/MatchesModel';
+import TeamsModel from '../database/models/TeamsModel';
+
 import matchesMock from './mocks/Matches';
 import matchesFinishedMock from './mocks/MatchesOnlyFinished';
 import matchesInProgress from './mocks/MatchesInProgress';
+import { newMatchesDTO, newMatchesInProgressMock } from './mocks/CreateMatches';
 
 import { Response } from 'superagent';
 import { IMatches } from '../interfaces/IMatches';
+import { teamsMock } from './mocks/Teams';
 
 chai.use(chaiHttp);
 
@@ -85,6 +89,36 @@ describe('/GET matches', () => {
       matches.forEach((value) => {
         expect(value.inProgress).to.eql(true);
       });
+    });
+  });
+});
+
+describe('/POST', () => {
+  describe('Sucess create match', () => {
+    beforeEach(async () => {
+      sinon
+        .stub(MatchesModel, 'create')
+        .resolves(newMatchesInProgressMock as MatchesModel);
+      sinon
+        .stub(TeamsModel, 'findByPk')
+        .onFirstCall()
+        .resolves(teamsMock[0] as TeamsModel)
+        .onSecondCall()
+        .resolves(teamsMock[1] as TeamsModel);
+    });
+
+    afterEach(() => {
+      (MatchesModel.create as sinon.SinonStub).restore();
+      (TeamsModel.findByPk as sinon.SinonStub).restore();
+    });
+
+    it('should return new match', async () => {
+      const response: Response = await chai
+        .request(app)
+        .post('/matches')
+        .send(newMatchesDTO);
+      expect(response.status).to.equal(201);
+      expect(response.body).to.have.keys(Object.keys(newMatchesInProgressMock));
     });
   });
 });
