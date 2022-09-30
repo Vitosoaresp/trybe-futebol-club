dc7a460e1396141f994111b88d3bedb8c67bc4df
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-30 16:11:15 -0300

    feat: controller create match

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -2,6 +2,8 @@ import { Response, Request } from 'express';
 
 import INTERNAL_ERROR from '../helpers/errorHelper';
 import getMatchesService from '../services/matches/getMatches';
+import createMatchesService from '../services/matches/createMatches';
+import { IMatchesDTO } from '../interfaces/IMatches';
 
 export default class MatchesController {
   static async getAll(req: Request, res: Response) {
@@ -14,6 +16,29 @@ export default class MatchesController {
         return res.status(code).json(data);
       }
       const { code, data } = await getMatchesService.getAll();
+      return res.status(code).json(data);
+    } catch (error) {
+      console.log(error);
+      res.status(INTERNAL_ERROR.code).json(INTERNAL_ERROR.message);
+    }
+  }
+  static async create(
+    req: Request<unknown, unknown, IMatchesDTO>,
+    res: Response,
+  ) {
+    try {
+      const { awayTeam, awayTeamGoals, homeTeam, homeTeamGoals, inProgress } =
+        req.body;
+      const { code, data, message } = await createMatchesService.create({
+        awayTeam,
+        awayTeamGoals,
+        homeTeam,
+        homeTeamGoals,
+        inProgress,
+      });
+      if (message) {
+        return res.status(code).json(message);
+      }
       return res.status(code).json(data);
     } catch (error) {
       console.log(error);
