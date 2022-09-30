9ebc3649bf7c7c47beaae27d2a07299ec83acd0b
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-30 16:12:12 -0300

    feat: router post match

--- app/backend/src/routes/MatchesRoutes.ts
+++ app/backend/src/routes/MatchesRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import MatchesController from '../controllers/MatchesController';
+import Auth from '../middleware/authToken';
 
 const router = Router();
 
 router.get('/matches', MatchesController.getAll);
+router.post('/matches', Auth.authToken, MatchesController.create);
 
 export default router;
