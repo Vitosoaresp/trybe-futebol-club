6058c70fba5f446e762cedd06b9fe36f9a8c7f14
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-30 17:35:20 -0300

    feat: controller match update

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -1,8 +1,10 @@
 import { Response, Request } from 'express';
 
-import INTERNAL_ERROR from '../helpers/errorHelper';
 import getMatchesService from '../services/matches/getMatches';
 import createMatchesService from '../services/matches/createMatches';
+import updateMatches from '../services/matches/updateMatches';
+
+import INTERNAL_ERROR from '../helpers/errorHelper';
 import { IMatchesDTO } from '../interfaces/IMatches';
 
 export default class MatchesController {
@@ -31,8 +33,18 @@ export default class MatchesController {
       const { code, data, message } = await createMatchesService.create(
         req.body,
       );
-      if (message) return res.status(code).json(message);
+      if (message) return res.status(code).json({ message });
       return res.status(code).json(data);
+    } catch (error) {
+      res.status(INTERNAL_ERROR.code).json(INTERNAL_ERROR.message);
+    }
+  }
+
+  static async update(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const { code, message } = await updateMatches.update(id);
+      return res.status(code).json({ message });
     } catch (error) {
       res.status(INTERNAL_ERROR.code).json(INTERNAL_ERROR.message);
     }
