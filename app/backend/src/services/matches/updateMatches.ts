b5817e70a9711f7ada6d837648555305466ad8bb
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-30 18:29:39 -0300

    feat: update goals match

--- app/backend/src/services/matches/updateMatches.ts
+++ app/backend/src/services/matches/updateMatches.ts
@@ -1,8 +1,16 @@
 import Matches from '../../database/models/MatchesModel';
 
 export default class updateMatches {
-  static async update(id: string) {
+  static async finished(id: string) {
     await Matches.update({ inProgress: false }, { where: { id } });
     return { code: 200, message: 'Finished' };
+  }
+
+  static async changeGoals(id: string, homeGoals: string, awayGoals: string) {
+    await Matches.update(
+      { homeTeamGoals: homeGoals, awayTeamGoals: awayGoals },
+      { where: { id } },
+    );
+    return { code: 200, message: 'Updated goals' };
   }
 }
