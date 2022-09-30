245e99d4d596b34760e93d9a2c7af6d041c79332
Vitor Soares <pereiravitor1218@gmail.com>
2022-09-29 21:02:40 -0300

    refac: use filter in getAll

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -4,8 +4,15 @@ import INTERNAL_ERROR from '../helpers/errorHelper';
 import getMatchesService from '../services/matches/getMatches';
 
 export default class MatchesController {
-  static async getAll(_req: Request, res: Response) {
+  static async getAll(req: Request, res: Response) {
     try {
+      const { inProgress } = req.query;
+      if (inProgress) {
+        const { data, code } = await getMatchesService.getByInProgress(
+          inProgress as string,
+        );
+        return res.status(code).json(data);
+      }
       const { code, data } = await getMatchesService.getAll();
       return res.status(code).json(data);
     } catch (error) {
